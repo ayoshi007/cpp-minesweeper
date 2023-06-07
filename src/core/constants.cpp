68ee904fd07bc5f1425cf10c8012ff87ac526f3f
Moka <mokapot21@proton.me>
2023-06-07 10:55:30 -0500

    Fix constants multiple definintion linking error

---
+++ src/core/constants.cpp
@@ -0,0 +1,24 @@
+#include <core/constants.hpp>
+
+namespace constants {
+    extern const int SMALL_BOARD_W {8};
+    extern const int SMALL_BOARD_H {8};
+    extern const int SMALL_BOARD_MINES {10};
+    extern const int MED_BOARD_W {16};
+    extern const int MED_BOARD_H {16};
+    extern const int MED_BOARD_MINES {40};
+    extern const int LARGE_BOARD_W {30};
+    extern const int LARGE_BOARD_H {16};
+    extern const int LARGE_BOARD_MINES {99};
+    extern const int DEFAULT_CUSTOM_W {30};
+    extern const int DEFAULT_CUSTOM_H {30};
+    extern const int DEFAULT_CUSTOM_PERCENT {15};
+
+    extern const int MAIN_MENU_SCREEN {0};
+    extern const int GAME_BOARD_SCREEN {1};
+    /*
+    extern int smallBoard[SMALL_BOARD_H][SMALL_BOARD_W] {};
+    extern int medBoard[MED_BOARD_H][MED_BOARD_W] {};
+    extern int largeBoard[LARGE_BOARD_H][LARGE_BOARD_W] {};
+    */
+}
