2b05b0dde865a6385abee3309e367010b9cae322
Moka <mokapot21@proton.me>
2023-06-12 16:08:26 -0500

    Remove FTXUI example and test builds from build process

--- build-debug.sh
+++ build-debug.sh
@@ -1,2 +1,2 @@
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
-cmake --build build
+cmake -S . -B build/debug -DCMAKE_BUILD_TYPE=Debug -DFTXUI_BUILD_EXAMPLES=OFF -DFTXUI_BUILD_TESTS=OFF
+cmake --build build/debug
