95b657001a5b01602e000d9a1ad5b4f029cf0cf5
Moka <mokapot21@proton.me>
2023-06-22 22:05:26 -0500

    Add more methods to header that will be needed by UI

--- src/game/controller.cpp
+++ src/game/controller.cpp
@@ -1,12 +1,19 @@
+#include <doctest/doctest.h>
 #include <game/controller.hpp>
 #include <game/board.hpp>
 
 namespace GameLogic {
-    BoardController::BoardController() : board {} {
-
+    BoardController::BoardController() {
     }
     bool BoardController::initialize_board(int width, int height, int mine_count, int seed) {
-        
+        if (width <= 0 || height <= 0 || mine_count <= 0) {
+            throw "Cannot accept negative arguments";
+        }
+        if (width * height <= mine_count) {
+            throw "Mine count cannot exceed total number of cells";
+        }
+        board = Board(width, height, mine_count, seed);
+        return true;
     }
     bool BoardController::initialize_board(int width, int height, int mine_count) {
         if (width <= 0 || height <= 0 || mine_count <= 0) {
@@ -15,17 +22,21 @@ namespace GameLogic {
         if (width * height <= mine_count) {
             throw "Mine count cannot exceed total number of cells";
         }
-        board.set_board(width, height, mine_count);
+        board = Board(width, height, mine_count);
         return true;
     }
-    bool BoardController::select(int x, int y) {
+    std::vector<std::pair<std::pair<int, int>, int>> BoardController::select(int x, int y) {
         if (x < 0 || x >= board.get_height()) {
             throw "Cannot accept negative arguments";
         }
         if (y < 0 || y >= board.get_width()) {
             throw "Cannot accept negative arguments";
         }
-        return board.select(x, y);
+        if (board.is_game_done()) {
+            throw "Game is over";
+        }
+        board.select(x, y);
+        return {};
     }
     bool BoardController::flag(int x, int y) {
         if (x < 0 || x >= board.get_height()) {
@@ -34,9 +45,35 @@ namespace GameLogic {
         if (y < 0 || y >= board.get_width()) {
             throw "Cannot accept negative arguments";
         }
+        if (board.is_game_done()) {
+            throw "Game is over";
+        }
         return board.flag(x, y);
     }
     int BoardController::get_flag_count()  {
         return board.get_flag_count();
+    }
+}
+
+TEST_SUITE("Board controller") {
+    TEST_CASE("Board controller creation") {
+        SUBCASE("Create controller") {
+            GameLogic::BoardController controller;
+            MESSAGE("Created controller");
+        }
+        SUBCASE("Create controller and initialize board") {
+            GameLogic::BoardController controller;
+            controller.initialize_board(8, 8, 10, 5);
+            MESSAGE("Created controller and initialized board with seed");
+        }
+        SUBCASE("Check erroneous initialize board arguments") {
+            GameLogic::BoardController controller;
+            SUBCASE("Negative args")  {
+                CHECK_THROWS(controller.initialize_board(-8, 8, 10));
+            }
+            SUBCASE("Too many mines") {
+                CHECK_THROWS(controller.initialize_board(8, 8, 64));
+            }
+        }
     }
 }
--- src/game/controller.hpp
+++ src/game/controller.hpp
@@ -13,10 +13,17 @@ namespace GameLogic {
 
         public:
         BoardController();
+        bool initialize_board(int width, int height, int mine_count, int seed);
         bool initialize_board(int width, int height, int mine_count);
-        bool select(int x, int y);
+        // selects a location on the board and returns all uncovered spots
+        // includes any uncovered mines (with value -1)
+        std::vector<std::pair<std::pair<int, int>, int>> select(int x, int y);
+        // flags a location and returns if the player won
         bool flag(int x, int y);
+        // gets the number of set flags
         int get_flag_count();
+        // gets all mine locations (only returns when the game is lost)
+        std::vector<std::pair<int, int>> get_mine_locations();
     };
 }
 
