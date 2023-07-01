57d15816f0d743b5711a02f63f61c9985e993799
Moka <mokapot21@proton.me>
2023-06-30 20:13:44 -0500

    Add struct BoardCell, class member game_board to store the state of the board's UI

--- src/ui/game_board.hpp
+++ src/ui/game_board.hpp
@@ -1,15 +1,34 @@
 #ifndef GAME_BOARD_HPP
 #define GAME_BOARD_HPP
 
-#include <ftxui/component/component.hpp>
 #include <game/controller.hpp>
+
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/component_options.hpp>
+#include <ftxui/component/component_base.hpp>
+#include <ftxui/component/mouse.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/dom/canvas.hpp>
 
 // define all components and elements of the game board here
 
 namespace GameUI {
+    struct BoardCell {
+        int row;
+        int col;
+        ftxui::Color toggle_color;
+        ftxui::Color def_color;
+        ftxui::Color outline_color;
+        bool toggle;
+        ftxui::Component renderer;
+    };
     class GameBoard {
         private:
         GameLogic::BoardController board_controller;
+        std::vector<std::vector<BoardCell>> game_board;
+        
         
     };
 }
