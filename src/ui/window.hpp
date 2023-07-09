ce435aef80593921967d214cce84baaa325ea74e
Moka <mokapot21@proton.me>
2023-07-09 12:02:44 -0500

    Add method to show the game given the chosen difficulty

--- src/ui/window.hpp
+++ src/ui/window.hpp
@@ -27,6 +27,7 @@ namespace GameUI {
         public:
            static void start();
            static void show_main_menu();
+           static void show_game(int w, int h, int mine_count);
     };
 }
 
